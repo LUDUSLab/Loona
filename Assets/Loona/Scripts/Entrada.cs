9124ba772bdc29d7e42e6fd310e64fe2be7b6f0c
Jhone Mendonça <g.m.enohj@gmail.com>
2015-10-23 15:36:42 -0300

    Bug fixed
    Fixed the shoot on pause button click

--- Assets/Loona/Scripts/Entrada.cs
+++ Assets/Loona/Scripts/Entrada.cs
@@ -5,40 +5,39 @@ public class Entrada : MonoBehaviour {
 
     private float vertical;
     private float horizontal;
-    private Vector3 touchDeltaPosition;
-    private Camera _camera;
     private Vector3 mousePos;
 	private bool flag = true;
-    public float TouchTime;
-	private GameObject GameController;
+    public float TouchTime = 0.5f;
 	[SerializeField] private GameObject Controller;
-	private GameObject player;
-	public float movimentPorcentReduction;
+    [SerializeField] private string GameControllerTag = "GameController";
+    private GameObject player;
+    [SerializeField] private string PlayerTag = "Player";
+    public float movimentPorcentReduction = 0.1f;
+    private GameObject PauseButton;
+    [SerializeField] private string PauseButtonTag = "PauseButton";
+    private float PauseButtonRange; //30f
+
     // Use this for initialization
     void Start () {
-        _camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-		//GameController = GameObject.FindGameObjectWithTag ("GameController");
-		player = GameObject.FindGameObjectWithTag ("Player");
+		Controller = GameObject.FindGameObjectWithTag (GameControllerTag);
+		player = GameObject.FindGameObjectWithTag (PlayerTag);
 		InvokeRepeating ("Update2", 0, 0.001f);
-		Controller = GameObject.FindGameObjectWithTag ("GameController");
-	}
+        PauseButton = GameObject.FindGameObjectWithTag(PauseButtonTag);
+        PauseButtonRange = PauseButton.GetComponent<RectTransform>().rect.width;
+    }
  
 	void Update2()
 	{
-		//<!--captura as coordenadas do toque-->
-		//horizontal = Input.GetAxis("Horizontal");
-		//vertical = Input.GetAxis("Vertical");
 		if (Input.GetButtonDown ("Fire1"))
 		{
+            mousePos = Input.mousePosition;
+            if (ButtonWasClicked(mousePos, PauseButton.gameObject.transform.position, PauseButtonRange))
+            {
+                return;       
+            }
 			if (flag)
 			{
 				flag = false;
-
-				mousePos = Input.mousePosition;
-				//mousePos.z = 0;
-				//mousePos = _camera.ScreenToWorldPoint(mousePos);
-					//ScreenToWorldPoint (mousePos);
-				//GameController.GetComponent<Crescer>().MassAddUp(player,movimentPorcentReduction);
 
 				if(player.gameObject.transform.localScale.x <= 0.2)
 				{
@@ -47,55 +46,39 @@ public class Entrada : MonoBehaviour {
 				player.transform.localScale -= new Vector3(movimentPorcentReduction,movimentPorcentReduction,1);
 				Camera.main.orthographicSize = Camera.main.orthographicSize - movimentPorcentReduction/2;
 
-
-
 				horizontal = mousePos.x - (int) Screen.width / 2;
 				vertical = mousePos.y - (int) Screen.height / 2;
 				
-				//Debug.Log ("horizontal: " + horizontal + "vertical: " + vertical);
-				//Debug.Log ("largura/altura da tela: " + Screen.width + "/ " + Screen.height);
-				//move a gosma de acordo com a entrada
 				GetComponent<Movimentacao>().Mover(horizontal, vertical, ForceMode2D.Impulse);
-				
-				//atira massa de acordo com a entrada
-				//Debug.Log("antes de atirar!1");
+
 				GetComponent<Tiro>().Atirar(horizontal, vertical, ForceMode2D.Force);
-                //Debug.Log("x/y:" + mousePos.x + "/" + mousePos.y);
+                
                 Invoke("SetFlag", TouchTime);
 			}
 		}
-		/*if (Input.touchCount == 1) {
-			if(Input.GetTouch(0).phase == TouchPhase.Ended)
-			{
-				mousePos = _camera.ScreenToWorldPoint (Input.GetTouch(0).position);
-				//touchDeltaPosition = Input.GetTouch(0).position;
-				
-				//horizontal = touchDeltaPosition.x;
-				//vertical = touchDeltaPosition.y;
-				horizontal = mousePos.x;
-				vertical = mousePos.y;
-				
-				Debug.Log ("horizontal: " + horizontal + "vertical: " + vertical);
-				
-				//move a gosma de acordo com a entrada
-				GetComponent<Movimentacao> ().Mover (horizontal, vertical, ForceMode2D.Impulse);
-				
-				//atira massa de acordo com a entrada
-				//Debug.Log("antes de atirar!1");
-				GetComponent<Tiro> ().Atirar (horizontal, vertical, ForceMode2D.Force);
-
-				//iniciar thread aqui para deixar flag true depois de alguns segundos
-				//flag = false;
-			}
-
-		}*/
 	}
 
-	// Update is called once per frame
-    void Update()
+    private bool ButtonWasClicked(Vector3 TouchPosition, Vector3 ButtonPosition, float ButtonSize)
     {
-        
+        if(TouchPosition.x < ButtonPosition.x - ButtonSize)
+        {
+            return false;
+        }
+        else if(TouchPosition.y < ButtonPosition.y - ButtonSize)
+        {
+            return false;
+        }
+        else if (TouchPosition.x > ButtonPosition.x + ButtonSize)
+        {
+            return false;
+        }
+        else if (TouchPosition.y > ButtonPosition.y + ButtonSize)
+        {
+            return false;
+        }
+        return true;
     }
+
     void SetFlag() {
          this.flag= true;
     }

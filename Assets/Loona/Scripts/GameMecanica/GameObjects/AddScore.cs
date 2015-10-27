9f92da5b775f5260dce07ca9751e3858c803ace9
Lucas <soluclea@gmail.com>
2015-10-27 12:21:26 -0400

    LoonaScore

---
+++ Assets/Loona/Scripts/GameMecanica/GameObjects/AddScore.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddScore : MonoBehaviour {
+    public int ScoreToAdd;
+    public string SceneAspectsControllerTag;
+    private GameObject SceneAspectsController;
+	// Use this for initialization
+	void Start () {
+        SceneAspectsController = GameObject.FindGameObjectWithTag(SceneAspectsControllerTag);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.tag == "ColisorPlayer")
+        {
+            SceneAspectsController.GetComponent<ScoreManager>().SetScore(ScoreToAdd);
+        }
+        GetComponent<Collider2D>().enabled = false;
+    }
+}
---
+++ Assets/Loona/Scripts/GameMecanica/SceneAspects/ScoreManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class ScoreManager : MonoBehaviour {
+
+    public string ScoreTAG, ScoreGameOver, HighScoreTAG;
+    private GameObject Score, ScoreGameOverr, HighScorer;
+    private int Scorer;
+    private int HighScore;
+    // Use this for initialization
+    void Start()
+    {
+        Score = GameObject.FindGameObjectWithTag(ScoreTAG);
+        ScoreGameOverr = GameObject.FindGameObjectWithTag(ScoreGameOver);
+        HighScorer = GameObject.FindGameObjectWithTag(HighScoreTAG);
+        getPlayerPrefs();
+        Score.GetComponent<Text>().text = "Score: " + Scorer;
+        ScoreGameOverr.GetComponent<Text>().text = " " + Scorer;
+        HighScorer.GetComponent<Text>().text = " " + HighScore;
+    }
+
+    // Update is called once per frame
+    public void SetScore(int ScoreToAdd)
+    {
+        Scorer = Scorer + ScoreToAdd;
+        Score.GetComponent<Text>().text = "Score: " + Scorer;
+        ScoreGameOverr.GetComponent<Text>().text = " " + Scorer;
+        HighScorer.GetComponent<Text>().text = " " + HighScore;
+        if (Scorer >= HighScore)
+        {
+            HighScore = Scorer;
+            setPlayerPrefs(Scorer);
+            PlayerPrefs.Save();
+        }
+    }
+    public int GetScore()
+    {
+        return Scorer;
+    }
+    void getPlayerPrefs()
+    {
+        HighScore = PlayerPrefs.GetInt("highscore");
+    }
+
+    void setPlayerPrefs(int score)
+    {
+        PlayerPrefs.SetInt("highscore", score);
+    }
+}

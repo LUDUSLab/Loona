26490b7e83ba6bd0c0b50fab7a921b2a842a7ee0
Jhone Mendonça <g.m.enohj@gmail.com>
2015-10-28 14:44:35 -0300

    Loona animations
    Loona animations (scary right and scary left)

---
+++ Assets/Loona/Scripts/ScaryToObjectDirection.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaryToObjectDirection : MonoBehaviour {
+
+    public string ObjectTag = "Enemy";
+    public GameObject PlayerExpressions;
+    public string ScaryLeftTrigger = "Enemy_Letf";
+    public string ScaryRightTrigger = "Enemy_Right";
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag(ObjectTag))
+        {
+            if(other.transform.position.x > this.transform.position.x)
+            {
+                PlayerExpressions.GetComponent<Animator>().SetTrigger(ScaryRightTrigger);
+                Debug.Log("scaryright");
+            }
+            else
+            {
+                PlayerExpressions.GetComponent<Animator>().SetTrigger(ScaryLeftTrigger);
+                Debug.Log("scaryleft");
+            }
+        }
+    }
+}

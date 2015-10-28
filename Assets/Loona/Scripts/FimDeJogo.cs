6d7e99eade282be770d83913d19acb15fc17956a
Jhone Mendonça <g.m.enohj@gmail.com>
2015-10-28 12:42:19 -0300

    Loona Death Animation
    Added Loona particle death animation

--- Assets/Loona/Scripts/FimDeJogo.cs
+++ Assets/Loona/Scripts/FimDeJogo.cs
@@ -4,6 +4,10 @@ using System.Collections;
 //Made By JMG
 public class FimDeJogo : MonoBehaviour {
 
+    public GameObject PlayerParticleDeathPrefab;
+    public GameObject PlayerObject;
+    public float DelayTimeGameOverPopUp = 0.5f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -16,8 +20,14 @@ public class FimDeJogo : MonoBehaviour {
 
     public void Morrer()
     {
+        PlayerObject.GetComponent<UnityJellySprite>().SetKinematic(true, false);
+        Instantiate(PlayerParticleDeathPrefab, PlayerObject.transform.position, Quaternion.identity);
+        Invoke("DelayGameOver", DelayTimeGameOverPopUp);
+    }
+
+    private void DelayGameOver()
+    {
         GetComponent<ButtonController>().SetTriggerAnimator("GameOver");
         Time.timeScale = 0;
-        Debug.Log("Derrotado");
     }
 }

c621d70ac50578b35b97b5750a95044acd9d034d
Jhone Mendonça <g.m.enohj@gmail.com>
2015-11-09 15:12:29 -0300

    Food spawn
    food spawn fixed (inside limits and away from planet)

--- Assets/Loona/Scripts/GameMecanica/ObjectsBehavior/MapObjectsSpawn.cs
+++ Assets/Loona/Scripts/GameMecanica/ObjectsBehavior/MapObjectsSpawn.cs
@@ -7,12 +7,15 @@ public class MapObjectsSpawn : MonoBehaviour
     [SerializeField] private int MaxAmountObjects;
     //food = 100, enemy = ?
     [SerializeField] private GameObject ObjectPrefab;
-    [SerializeField] private float HorizontalMaxCoordinate = 30;
-    [SerializeField] private float VerticalMaxCoordinate = 15;
+    public float HorizontalMaxCoordinate = 42;
+    public float VerticalMaxCoordinate = 21;
+    public float HorizontalMinCoordinate = -24;
+    public float VerticalMinCoordinate = -21;
     [SerializeField] private string ObjectsParentTag;
     private GameObject ObjectsParentObject;
     private GameObject Object;
     private Vector3 vector3 = new Vector3();
+    public GameObject PlanetObject;
 
     // Use this for initialization
     void Start()
@@ -22,21 +25,47 @@ public class MapObjectsSpawn : MonoBehaviour
     }
     public void InstantiateMapObjects(int amount, GameObject ObjectPrefabParam)
     {
+        CircleCollider2D PlanetObjectSize = PlanetObject.GetComponent<CircleCollider2D>();
+        float PlanetXMin = PlanetObjectSize.transform.position.x - PlanetObjectSize.radius + 1;
+        float PlanetXMax = PlanetObjectSize.transform.position.x + PlanetObjectSize.radius + 1;
+        float PlanetYMin = PlanetObjectSize.transform.position.y - PlanetObjectSize.radius + 1;
+        float PlanetYMax = PlanetObjectSize.transform.position.y + PlanetObjectSize.radius + 1;
+
         for (int i = 0; i < amount; i++)
         {
             Object = Instantiate(ObjectPrefab);
 
-            vector3.x = (int)Random.Range(-HorizontalMaxCoordinate, HorizontalMaxCoordinate);
+            vector3 = new Vector3((int)Random.Range(HorizontalMinCoordinate, HorizontalMaxCoordinate),
+                (int)Random.Range(VerticalMinCoordinate, VerticalMaxCoordinate),
+                0);
+            while (true)
+            {   
+                if((vector3.x < PlanetXMin || vector3.x > PlanetXMax) && (vector3.y < PlanetYMin || vector3.y > PlanetYMax))
+                {
+                    if (vector3.x != 0 && vector3.y != 0)
+                    {
+                        break;
+                    }
+                }
+                vector3 = new Vector3((int)Random.Range(HorizontalMinCoordinate, HorizontalMaxCoordinate),
+                (int)Random.Range(VerticalMinCoordinate, VerticalMaxCoordinate),
+                0);
+            }
+
+
+
+
+            /*vector3.x = (int)Random.Range(HorizontalMinCoordinate, HorizontalMaxCoordinate);
             while (vector3.x == 0)
             {
-                vector3.x = (int)Random.Range(-HorizontalMaxCoordinate, HorizontalMaxCoordinate);
+                vector3.x = (int)Random.Range(HorizontalMinCoordinate, HorizontalMaxCoordinate);
             }
-            vector3.y = (int)Random.Range(-VerticalMaxCoordinate, VerticalMaxCoordinate);
+            vector3.y = (int)Random.Range(VerticalMinCoordinate, VerticalMaxCoordinate);
             while (vector3.y == 0)
             {
-                vector3.y = (int)Random.Range(-VerticalMaxCoordinate, VerticalMaxCoordinate);
-            }
-            vector3.z = 0;
+                vector3.y = (int)Random.Range(VerticalMinCoordinate, VerticalMaxCoordinate);
+            }*/
+            //vector3.z = 0;
             Object.transform.position = vector3;
             Object.transform.parent = ObjectsParentObject.transform;
         }

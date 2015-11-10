7330e120826aa6185a58b6993f63aa679bde07a5
YonHoshihara <hoshiharyon@gmail.com>
2015-11-10 19:35:14 -0300

    Character Selection Bug corrigido
    Bug loona é selecionada com um toque corrigido

--- Assets/Loona/Scripts/Menus/SelectedPlayer.cs
+++ Assets/Loona/Scripts/Menus/SelectedPlayer.cs
@@ -4,18 +4,35 @@ using System.Collections;
 public class SelectedPlayer : MonoBehaviour {
     public int PositionInVector;
     public string LeveltoLoad;
+   // private int conttouch= 0 ;
     // Use this for initialization
 
-void OnMouseDown()
+void OnMouseDrag()
     {
         PlayerPrefs.SetInt("PositionInCharacterVector", PositionInVector);
+        Debug.Log(PositionInVector);
+       
+    }
+
+    void OnMouseUpAsButton()
+    {
         Application.LoadLevel(LeveltoLoad);
     }
-   // void Start () {
-	
-	//}
+    //void OnMouseExit()
+    // {
+
+    //  Application.LoadLevel(LeveltoLoad);
+
+    // }
+
+
+
+
+    // void Start () {
+
+    //}
     // Update is called once per frame
-	//void Update () {
-	
-	//}
+    //void Update () {
+
+    //}
 }

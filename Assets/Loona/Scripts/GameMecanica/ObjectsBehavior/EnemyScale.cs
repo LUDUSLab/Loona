163b63ead9961b76091ad4f341f8ff09a8362b8d
YonHoshihara <hoshiharyon@gmail.com>
2015-12-04 21:55:12 -0400

    ENEMY scale
    Enemy Scale

--- Assets/Loona/Scripts/GameMecanica/ObjectsBehavior/EnemyScale.cs
+++ Assets/Loona/Scripts/GameMecanica/ObjectsBehavior/EnemyScale.cs
@@ -3,12 +3,12 @@ using System.Collections;
 
 public class EnemyScale : MonoBehaviour {
 	private GameObject PlayerController;
-	public float ScaleAddOnStart,divisor;
+	public float ScaleLoonaAddedOnEnemy;
 	// Use this for initialization
 	void Start () {
 		PlayerController = GameObject.FindGameObjectWithTag ("PlayerController");
 		if(transform.localScale.x < PlayerController.GetComponent<ScaleTrack>().CheckPlayerSize().x ){
-			transform.localScale =  new Vector3(PlayerController.GetComponent<ScaleTrack>().CheckPlayerSize().x/divisor ,PlayerController.GetComponent<ScaleTrack>().CheckPlayerSize().y/divisor ,0);
+			transform.localScale =  new Vector3(PlayerController.GetComponent<ScaleTrack>().CheckPlayerSize().x*ScaleLoonaAddedOnEnemy ,PlayerController.GetComponent<ScaleTrack>().CheckPlayerSize().y*ScaleLoonaAddedOnEnemy ,0);
 			if(transform.localScale.x > 2){
 				transform.localScale = new Vector3(2f,2f,0f);
 

a3039a5b1e84cf431a00cb94c2c40a7f9631a06d
YonHoshihara <hoshiharyon@gmail.com>
2016-03-27 17:41:11 -0300

    Directon Arrow
    A directional Arrow is actived when loona can eat the planet

---
+++ Assets/LookAt.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAt : MonoBehaviour {
+    public Transform target,arrow;
+    private Vector3 direction;
+    private float angle;
+    private Quaternion rotation;
+    public float rotationSpeed;
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("MissionObjective").transform;
+        arrow = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+     void FixedUpdate()
+    { 
+        direction = target.position-arrow.transform.position;
+        angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+        //Debug.Log(direction)
+    }
+}
--- Assets/Loona/Scripts/GameMecanica/SceneAspects/ArrowAtivation.cs
+++ Assets/Loona/Scripts/GameMecanica/SceneAspects/ArrowAtivation.cs
@@ -1,39 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class VictoryCondition : MonoBehaviour {
+public class ArrowAtivation : MonoBehaviour {
+    public GameObject Arrow;
     public float TimeToCheckSize;
-	public float ScaleOffset;
+    public float ScaleOffset;
     private GameObject MissionObjective;
     private GameObject PlayerController;
     private Vector3 MissionObjectiveSize;
-    public GameObject Arrow;
+
     // Use this for initialization
     void Start () {
         MissionObjective = GameObject.FindGameObjectWithTag("MissionObjective");
-        PlayerController=  GameObject.FindGameObjectWithTag("PlayerController");
+        PlayerController = GameObject.FindGameObjectWithTag("PlayerController");
         MissionObjectiveSize = MissionObjective.GetComponent<CircleCollider2D>().bounds.size;
-		PlayerController.GetComponent<Crescer> ().MaxSize = MissionObjectiveSize.x + ScaleOffset; // Tamanho máximo
+        PlayerController.GetComponent<Crescer>().MaxSize = MissionObjectiveSize.x + ScaleOffset; // Tamanho máximo
         InvokeRepeating("CompareSize", 1, TimeToCheckSize);
     }
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
-    void CompareSize()
-    {
+	void FixedUpdate () {
         Vector3 PlayerSize = PlayerController.GetComponent<ScaleTrack>().CheckPlayerSize();
-		if (MissionObjectiveSize.x + ScaleOffset> PlayerSize.x) //Tem que comparar pelo tamanho em X deles(diametro)
+        if (MissionObjectiveSize.x + ScaleOffset > PlayerSize.x) //Tem que comparar pelo tamanho em X deles(diametro)
         {
             MissionObjective.GetComponent<CircleCollider2D>().isTrigger = false;
-            Arrow.SetActive(false);
+
+
         }
         else
         {
             MissionObjective.GetComponent<CircleCollider2D>().isTrigger = true;
             Arrow.SetActive(true);
-
         }
+
     }
 }

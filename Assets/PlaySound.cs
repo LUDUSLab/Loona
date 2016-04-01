6917c5c3676e449867dca7ce59fcd5b67f3c0b12
Jhone Mendonça <g.m.enohj@gmail.com>
2016-04-01 16:48:46 -0400

    Buttons Sound

---
+++ Assets/PlaySound.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySound : MonoBehaviour {
+
+    [SerializeField]
+    private AudioClip SoundClip;
+    [SerializeField]
+    private AudioSource source;
+
+    // Use this for initialization
+    void Start () {
+        source = GameObject.Find("Audio Source").GetComponent<AudioSource>();
+    }
+	
+    public void Play()
+    {
+        source.PlayOneShot(SoundClip);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

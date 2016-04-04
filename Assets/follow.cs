5d4f78564140a7be4d6789847502c680eda33b5e
YonHoshihara <hoshiharyon@gmail.com>
2016-04-04 14:57:19 -0400

    Loona rotation arrow Fixed
    Loona rotation arrow Fixed

---
+++ Assets/follow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class follow : MonoBehaviour {
+
+    // Use this for initialization
+
+    public GameObject target;
+
+
+    void Start () {
+  
+	}
+    // Update is called once per frame
+	void Update () {
+        transform.position = target.transform.position;
+	}
+}
